27b97c0312c1c56bd609939582946c68d94d7286
Weinan Li <weli@redhat.com>
2011-09-09 20:53:54 +0800

    add TestInMemoryClientExecutor

---
+++ src/test/java/org/bluedash/resteasy/test/integration/test/TestInMemoryClientExecutor.java
@@ -0,0 +1,11 @@
+package org.bluedash.resteasy.test.integration.test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: liweinan
+ * Date: 9/9/11
+ * Time: 8:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestInMemoryClientExecutor {
+}
