814461abc0e70887575982707736b9ae1d549cdd
Weinan Li <weli@redhat.com>
2011-09-11 01:28:01 +0800

    cleanup files

--- src/test/java/org/bluedash/resteasy/test/integration/test/TestInMemoryClientExecutor.java
+++ src/test/java/org/bluedash/resteasy/test/integration/test/TestInMemoryClientExecutor.java
@@ -1,11 +1,37 @@
 package org.bluedash.resteasy.test.integration.test;
 
-/**
- * Created by IntelliJ IDEA.
- * User: liweinan
- * Date: 9/9/11
- * Time: 8:43 PM
- * To change this template use File | Settings | File Templates.
- */
+import junit.framework.Assert;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.core.executors.InMemoryClientExecutor;
+import org.junit.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 public class TestInMemoryClientExecutor {
+
+    public static final String HELLO_WORLD = "Hello, world!";
+    @Path("/helloworld")
+    public static class HelloWorldService {
+
+        @GET
+        @Produces("text/plain")
+        public String printHelloWorld() {
+            return HELLO_WORLD;
+        }
+    }
+
+    @Test
+    public void testHelloWorld() {
+        InMemoryClientExecutor executor = new InMemoryClientExecutor();
+        executor.getDispatcher().getRegistry().addPerRequestResource(HelloWorldService.class);
+        ClientRequest request = new ClientRequest("/helloworld", executor);
+        try {
+            Assert.assertEquals(HELLO_WORLD, request.getTarget(String.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/test/java/org/bluedash/resteasy/test/integration/test/TestUserAPI.java
+++ src/test/java/org/bluedash/resteasy/test/integration/test/TestUserAPI.java
@@ -1,32 +1,32 @@
 package org.bluedash.resteasy.test.integration.test;
 
+import junit.framework.TestCase;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import junit.framework.TestCase;
-
 public class TestUserAPI extends TestCase {
-	public static final String USER_API = "http://127.0.0.1:8080/try-resteasy/users";
+    public static final String USER_API = "http://127.0.0.1:8080/try-resteasy/users";
 
-	public void testCreateUserAndGetUser() throws IOException {
-		URL url = new URL(USER_API);
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setRequestMethod("POST");
-		connection.setRequestProperty("Content-Type", "application/xml");
-		connection.setDoOutput(true);
-		connection.setInstanceFollowRedirects(false);
-		connection.setConnectTimeout(1000);
+    public void testCreateUserAndGetUser() throws IOException {
+        URL url = new URL(USER_API);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/xml");
+        connection.setDoOutput(true);
+        connection.setInstanceFollowRedirects(false);
+        connection.setConnectTimeout(1000);
 
-		String userXML = "<user><name>liweinan</name></user>";
-		OutputStream os = connection.getOutputStream();
-		os.write(userXML.getBytes());
-		os.flush();
+        String userXML = "<user><name>liweinan</name></user>";
+        OutputStream os = connection.getOutputStream();
+        os.write(userXML.getBytes());
+        os.flush();
 
-		assertEquals(HttpURLConnection.HTTP_CREATED,
-				connection.getResponseCode());
-		connection.disconnect();
+        assertEquals(HttpURLConnection.HTTP_CREATED,
+                connection.getResponseCode());
+        connection.disconnect();
 
-	}
+    }
 }
