163c305b89ccd63a6d2a8051ffe5cccf07ff0fb4
Weinan Li <l.weinan@gmail.com>
2012-10-17 03:07:47 +0800

    modified fix

--- src/main/java/net/bluedash/resteasy/Entry.java
+++ src/main/java/net/bluedash/resteasy/Entry.java
@@ -43,7 +43,7 @@ import java.util.List;
  * element can appear as a child of the atom:feed element, or it can
  * appear as the document (i.e., top-level) element of a stand-alone
  * Atom Entry Document.
- * <p/>
+ *
  * atomEntry =
  *    element atom:entry {
  *       atomCommonAttributes,
@@ -61,13 +61,13 @@ import java.util.List;
  *        &amp; atomUpdated
  *        &amp; extensionElement*)
  *    }
- * <p/>
+ *
  * This specification assigns no significance to the order of appearance
  * of the child elements of atom:entry.
- * <p/>
+ *
  * The following child elements are defined by this specification (note
  * that it requires the presence of some of these elements):
- * <p/>
+ *
  * o  atom:entry elements MUST contain one or more atom:author elements,
  *    unless the atom:entry contains an atom:source element that
  *    contains an atom:author element or, in an Atom Feed Document, the
@@ -106,171 +106,148 @@ import java.util.List;
  * o  atom:entry elements MUST contain exactly one atom:title element.
  * o  atom:entry elements MUST contain exactly one atom:updated element.
  * </pre>
- *
+ * <p/>
  * TODO remove this file when JBoss AS includes RESTEasy 2.3.4.Final or higher
  */
-@XmlRootElement(namespace = "http://www.w3.org/2005/Atom" ,name = "entry")
+@XmlRootElement(namespace = "http://www.w3.org/2005/Atom", name = "entry")
 @XmlAccessorType(XmlAccessType.PROPERTY)
 @XmlType(propOrder = {"title", "links", "categories", "updated", "id", "published", "authors", "contributors", "source",
-        "rights", "content", "summary", "anyOther"})
+        "rights", "content", "summary", "anyOther" })
 @XmlSeeAlso(AtomAssetMetadata.class)
-public class Entry extends CommonAttributes
-{
-   private List<Person> authors = new ArrayList<Person>();
+public class Entry extends CommonAttributes {
+    private List<Person> authors = new ArrayList<Person>();
 
-   private List<Category> categories = new ArrayList<Category>();
+    private List<Category> categories = new ArrayList<Category>();
 
-   private Content content;
+    private Content content;
 
-   private List<Person> contributors = new ArrayList<Person>();
+    private List<Person> contributors = new ArrayList<Person>();
 
-   private URI id;
+    private URI id;
 
-   private List<Link> links = new ArrayList<Link>();
+    private List<Link> links = new ArrayList<Link>();
 
-   private Date published;
+    private Date published;
 
-   private String title;
+    private String title;
 
-   private Date updated;
+    private Date updated;
 
-   private String rights;
+    private String rights;
 
-   private Source source;
+    private Source source;
 
-   private String summary;
+    private String summary;
 
-   private Element anyOtherElement;
+    private Element anyOtherElement;
 
 
-   private List<Object> anyOther;
+    private List<Object> anyOther;
 
-   private Object anyOtherJaxbObject;
+    private Object anyOtherJaxbObject;
 
-   protected JAXBContextFinder finder;
+    protected JAXBContextFinder finder;
 
-   protected void setFinder(JAXBContextFinder finder)
-   {
-      this.finder = finder;
-   }
+    protected void setFinder(JAXBContextFinder finder) {
+        this.finder = finder;
+    }
 
-   @XmlElement(namespace = "http://www.w3.org/2005/Atom")
-   public URI getId()
-   {
-      return id;
-   }
+    @XmlElement(namespace = "http://www.w3.org/2005/Atom")
+    public URI getId() {
+        return id;
+    }
 
-   public void setId(URI id)
-   {
-      this.id = id;
-   }
+    public void setId(URI id) {
+        this.id = id;
+    }
 
-   @XmlElement(namespace = "http://www.w3.org/2005/Atom")
-   public String getTitle()
-   {
-      return title;
-   }
+    @XmlElement(namespace = "http://www.w3.org/2005/Atom")
+    public String getTitle() {
+        return title;
+    }
 
-   public void setTitle(String title)
-   {
-      this.title = title;
-   }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-   @XmlElement(namespace = "http://www.w3.org/2005/Atom")
-   public Date getUpdated()
-   {
-      return updated;
-   }
+    @XmlElement(namespace = "http://www.w3.org/2005/Atom")
+    public Date getUpdated() {
+        return updated;
+    }
 
-   public void setUpdated(Date updated)
-   {
-      this.updated = updated;
-   }
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
 
-   public Link getLinkByRel(String name)
-   {
-      for (Link link : links) if (link.getRel().equals(name)) return link;
-      return null;
-   }
+    public Link getLinkByRel(String name) {
+        for (Link link : links) if (link.getRel().equals(name)) return link;
+        return null;
+    }
 
-   @XmlElementRef(namespace = "http://www.w3.org/2005/Atom")
-   public List<Link> getLinks()
-   {
-      return links;
-   }
+    @XmlElementRef(namespace = "http://www.w3.org/2005/Atom")
+    public List<Link> getLinks() {
+        return links;
+    }
 
-   @XmlElementRef(namespace = "http://www.w3.org/2005/Atom")
-   public Content getContent()
-   {
-      return content;
-   }
+    @XmlElementRef(namespace = "http://www.w3.org/2005/Atom")
+    public Content getContent() {
+        return content;
+    }
 
-   public void setContent(Content content)
-   {
-      this.content = content;
-   }
+    public void setContent(Content content) {
+        this.content = content;
+    }
 
-   @XmlElement(namespace = "http://www.w3.org/2005/Atom", name = "author")
-   public List<Person> getAuthors()
-   {
-      return authors;
-   }
+    @XmlElement(namespace = "http://www.w3.org/2005/Atom", name = "author")
+    public List<Person> getAuthors() {
+        return authors;
+    }
 
-   @XmlElementRef(namespace = "http://www.w3.org/2005/Atom")
-   public List<Category> getCategories()
-   {
-      return categories;
-   }
+    @XmlElementRef(namespace = "http://www.w3.org/2005/Atom")
+    public List<Category> getCategories() {
+        return categories;
+    }
 
-   @XmlElement(namespace = "http://www.w3.org/2005/Atom", name = "contributor")
-   public List<Person> getContributors()
-   {
-      return contributors;
-   }
+    @XmlElement(namespace = "http://www.w3.org/2005/Atom", name = "contributor")
+    public List<Person> getContributors() {
+        return contributors;
+    }
 
-   @XmlElement(namespace = "http://www.w3.org/2005/Atom")
-   public Date getPublished()
-   {
-      return published;
-   }
+    @XmlElement(namespace = "http://www.w3.org/2005/Atom")
+    public Date getPublished() {
+        return published;
+    }
 
-   public void setPublished(Date published)
-   {
-      this.published = published;
-   }
+    public void setPublished(Date published) {
+        this.published = published;
+    }
 
-   @XmlElement(namespace = "http://www.w3.org/2005/Atom")
-   public String getRights()
-   {
-      return rights;
-   }
+    @XmlElement(namespace = "http://www.w3.org/2005/Atom")
+    public String getRights() {
+        return rights;
+    }
 
-   public void setRights(String rights)
-   {
-      this.rights = rights;
-   }
+    public void setRights(String rights) {
+        this.rights = rights;
+    }
 
-   @XmlElement(namespace = "http://www.w3.org/2005/Atom")
-   public Source getSource()
-   {
-      return source;
-   }
+    @XmlElement(namespace = "http://www.w3.org/2005/Atom")
+    public Source getSource() {
+        return source;
+    }
 
-   public void setSource(Source source)
-   {
-      this.source = source;
-   }
+    public void setSource(Source source) {
+        this.source = source;
+    }
 
-   @XmlElement(namespace = "http://www.w3.org/2005/Atom")
-   public String getSummary()
-   {
-      return summary;
-   }
+    @XmlElement(namespace = "http://www.w3.org/2005/Atom")
+    public String getSummary() {
+        return summary;
+    }
 
-   public void setSummary(String summary)
-   {
-      this.summary = summary;
-   }
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
 
     @Override
     public String toString() {
@@ -294,102 +271,105 @@ public class Entry extends CommonAttributes
     }
 
     /**
-    * Get content as an XML Element if the content is XML.  Otherwise, this will just return null.
-    *
-    * @return
-    */
-   @XmlTransient
-   public Element getAnyOtherElement()
-   {
-      if (anyOther == null) return null;
-      if (anyOtherElement != null) return anyOtherElement;
-      for (Object obj : anyOther)
-      {
-         if (obj instanceof Element)
-         {
-             anyOtherElement = (Element) obj;
-            return anyOtherElement;
-         }
-      }
-      return null;
-   }
+     * Get content as an XML Element if the content is XML.  Otherwise, this will just return null.
+     *
+     * @return
+     */
+    @XmlTransient
+    public Element getAnyOtherElement() {
+        if (anyOther == null) return null;
+        if (anyOtherElement != null) return anyOtherElement;
+        for (Object obj : anyOther) {
+            if (obj instanceof Element) {
+                anyOtherElement = (Element) obj;
+                return anyOtherElement;
+            }
+        }
+        return null;
+    }
 
-   @XmlMixed
-   @XmlAnyElement(lax = true)
-   public List<Object> getAnyOther() {
-       if (anyOther == null) {
-           anyOther = new ArrayList<Object>();
-       }
-       return this.anyOther;
-   }
-   /**
-    * Extract the content as the provided JAXB annotated type.
-    * <p/>
-    * This method will use a cached JAXBContext used by the Resteasy JAXB providers
-    * or, if those are not existent, it will create a new JAXBContext from scratch
-    * using the class.
-    *
-    * @param clazz                class type you are expecting
-    * @param otherPossibleClasses Other classe you want to create the JAXBContext with
-    * @return null if there is no XML content
-    * @throws JAXBException
-    */
-   public <T> T getAnyOtherJAXBObject(Class<T> clazz, Class... otherPossibleClasses) throws JAXBException {
-       JAXBContext ctx = null;
-       Class[] classes = {clazz};
-       if (otherPossibleClasses != null && otherPossibleClasses.length > 0) {
-           classes = new Class[1 + otherPossibleClasses.length];
-           classes[0] = clazz;
-           for (int i = 0; i < otherPossibleClasses.length; i++) classes[i + 1] = otherPossibleClasses[i];
-       }
-       if (finder != null) {
-           ctx = finder.findCacheContext(MediaType.APPLICATION_XML_TYPE, null, classes);
-       } else {
-           ctx = JAXBContext.newInstance(classes);
-       }
-       if (getAnyOther().size() == 0) return null;
-
-       Object obj = null;
-
-       if (getAnyOtherElement() != null) {
-           ctx.createUnmarshaller().unmarshal(getAnyOtherElement());
-       } else {
-           obj = getAnyOther().get(0);
-       }
-
-       if (obj instanceof JAXBElement) {
-           anyOtherJaxbObject = ((JAXBElement) obj).getValue();
-           return (T) anyOtherJaxbObject;
-       } else {
-           anyOtherJaxbObject = obj;
-           return (T) obj;
-       }
-   }
+    @XmlMixed
+    @XmlAnyElement(lax = true)
+    public List<Object> getAnyOther() {
+        if (anyOther == null) {
+            anyOther = new ArrayList<Object>();
+        }
+        return this.anyOther;
+    }
 
     /**
-    * Returns previous extracted jaxbobject from a call to getJAXBObject(Class<T> clazz)
-    * or value passed in through a previous setJAXBObject().
-    *
-    * @return
-    */
-   @XmlTransient
-   public Object getAnyOtherJAXBObject()
-   {
-      return anyOtherJaxbObject;
-   }
+     * Extract the content as the provided JAXB annotated type.
+     * <p/>
+     * This method will use a cached JAXBContext used by the Resteasy JAXB providers
+     * or, if those are not existent, it will create a new JAXBContext from scratch
+     * using the class.
+     *
+     * @param clazz                class type you are expecting
+     * @param otherPossibleClasses Other classe you want to create the JAXBContext with
+     * @return null if there is no XML content
+     * @throws JAXBException
+     */
+    public <T> T getAnyOtherJAXBObject(Class<T> clazz, Class... otherPossibleClasses) throws JAXBException {
+        JAXBContext ctx = null;
+        Class[] classes = {clazz};
+        if (otherPossibleClasses != null && otherPossibleClasses.length > 0) {
+            classes = new Class[1 + otherPossibleClasses.length];
+            classes[0] = clazz;
+            for (int i = 0; i < otherPossibleClasses.length; i++) classes[i + 1] = otherPossibleClasses[i];
+        }
+        if (finder != null) {
+            ctx = finder.findCacheContext(MediaType.APPLICATION_XML_TYPE, null, classes);
+        } else {
+            ctx = JAXBContext.newInstance(classes);
+        }
 
-   public void setAnyOtherJAXBObject(Object obj)
-   {
-      if (anyOther == null) anyOther = new ArrayList();
-      if (anyOtherJaxbObject != null && anyOther != null) anyOther.clear();
-      if (!obj.getClass().isAnnotationPresent(XmlRootElement.class) && obj.getClass().isAnnotationPresent(XmlType.class))
-      {
-          anyOther.add(JAXBXmlTypeProvider.wrapInJAXBElement(obj, obj.getClass()));
-      }
-      else
-      {
-          anyOther.add(obj);
-      }
-      anyOtherJaxbObject = obj;
-   }
+        Object obj = null;
+
+        if (getAnyOtherElement() != null) {
+            obj = ctx.createUnmarshaller().unmarshal(getAnyOtherElement());
+        } else {
+            if (getAnyOther().size() == 0) return null;
+            for (Object _obj : getAnyOther()) {
+                for (Class _clazz : classes) {
+                    if (_obj.getClass().equals(_clazz)) {
+                        obj = _obj;
+                        break;
+                    }
+                }
+            }
+
+            if (obj == null)
+                return null;
+        }
+
+        if (obj instanceof JAXBElement) {
+            anyOtherJaxbObject = ((JAXBElement) obj).getValue();
+            return (T) anyOtherJaxbObject;
+        } else {
+            anyOtherJaxbObject = obj;
+            return (T) obj;
+        }
+    }
+
+    /**
+     * Returns previous extracted jaxbobject from a call to getJAXBObject(Class<T> clazz)
+     * or value passed in through a previous setJAXBObject().
+     *
+     * @return
+     */
+    @XmlTransient
+    public Object getAnyOtherJAXBObject() {
+        return anyOtherJaxbObject;
+    }
+
+    public void setAnyOtherJAXBObject(Object obj) {
+        if (anyOther == null) anyOther = new ArrayList();
+        if (anyOtherJaxbObject != null && anyOther != null) anyOther.clear();
+        if (!obj.getClass().isAnnotationPresent(XmlRootElement.class) && obj.getClass().isAnnotationPresent(XmlType.class)) {
+            anyOther.add(JAXBXmlTypeProvider.wrapInJAXBElement(obj, obj.getClass()));
+        } else {
+            anyOther.add(obj);
+        }
+        anyOtherJaxbObject = obj;
+    }
 }
