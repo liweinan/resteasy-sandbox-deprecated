2fda3ccc07934fadb9df218671ebce17ce05a6c1
Weinan Li <weli@redhat.com>
2013-02-23 18:00:48 +0800

    cleanup

---
+++ src/main/java/net/bluedash/resteasy/BluedashApplication.java
@@ -0,0 +1,7 @@
+package net.bluedash.resteasy;
+
+/**
+ * @author <a href="mailto:l.weinan@gmail.com">Weinan Li</a>
+ */
+public class BluedashApplication extends Application {
+}
---
+++ src/test/java/net/bluedash/resteasy/test/JsonTest.java
@@ -0,0 +1,7 @@
+package net.bluedash.resteasy.test;
+
+/**
+ * @author <a href="mailto:l.weinan@gmail.com">Weinan Li</a>
+ */
+public class JsonTest {
+}
